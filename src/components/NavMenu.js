c82de360cc79cef47b431be10f136dc01e2fea23
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-02-25 00:15:28 +0300

    create style navMenu )

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
+import {MdMenu, MdClose} from "react-icons/md";
 
 const NavMenuStyle = styled.div`
   position: fixed;
@@ -10,19 +11,23 @@ const NavMenuStyle = styled.div`
   width: 100%;
   padding: 1rem 0;
   background: var(--dark-bg);
+
   ul {
     max-width: 1200px;
     margin: 0 auto;
     width: 90%;
     text-align: center;
+
     li {
       display: inline-block;
       border-radius: 8px;
       transition: 0.3s ease background-color;
+
       &:hover {
         background-color: var(--deep-dark);
       }
     }
+
     a {
       display: inline-block;
       font-family: 'RobotoMono Regular';
@@ -31,19 +36,108 @@ const NavMenuStyle = styled.div`
       color: var(--gray-1);
       outline: none;
     }
+
     .active {
       color: var(--white);
     }
   }
+
+  .mobile-menu-icon {
+    position: absolute;
+    right: 1rem;
+    top: 1rem;
+    width: 4rem;
+    cursor: pointer;
+    display: none;
+    outline: none;
+
+    * {
+      pointer-events: none;
+    }
+  }
+
+  .navItems .closeNavIcon {
+    display: none;
+  }
+
+  @media only screen and (max-width: 768px) {
+    padding: 0;
+    .hide-item {
+      transform: translateY(calc(-100% - var(--top)));
+    }
+
+    .mobile-menu-icon {
+      display: block;
+    }
+
+    .navItems {
+      --top: 1rem;
+      transition: 0.3s ease transform;
+      background-color: var(--deep-dark);
+      padding: 2rem;
+      width: 90%;
+      max-width: 300px;
+      border-radius: 12px;
+      position: absolute;
+      right: 1rem;
+      top: var(--top);
+
+      .closeNavIcon {
+        display: block;
+        width: 3rem;
+        margin: 0 0 0 auto;
+        cursor: pointer;
+
+        * {
+          pointer-events: none;
+        }
+      }
+
+      li {
+        display: block;
+        margin-bottom: 1rem;
+      }
+    }
+  }
 `
 export const NavMenu = () => {
+    const [showNav, setShowNav] = useState(false)
     return (
         <NavMenuStyle>
-            <ul>
-                <li><Link to='/' exact >Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/projects'>Projects</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
+            <div   className="mobile-menu-icon"
+                   onClick={() => setShowNav(!showNav)}
+                   role="button"
+                   onKeyDown={() => setShowNav(!showNav)}
+                   tabIndex={0}>
+                <MdMenu/>
+            </div>
+            <ul className={!showNav ? 'navItems hide-item' : 'navItems'}>
+                <div className='closeNavIcon'
+                onClick={()=>{setShowNav(!showNav)}}
+                role='button'
+                onKeyDown={()=>{setShowNav(!showNav)}}
+                tabIndex={0}>
+                    <MdClose/>
+                </div>
+                <li><Link to='/' exact="true"
+                          onClick={() => setShowNav(!showNav)}
+                          role="button"
+                          onKeyDown={() => setShowNav(!showNav)}
+                          tabIndex={0}>Home</Link></li>
+                <li><Link to='/about'
+                          onClick={() => setShowNav(!showNav)}
+                          role="button"
+                          onKeyDown={() => setShowNav(!showNav)}
+                          tabIndex={0}>About</Link></li>
+                <li><Link to='/projects'
+                          onClick={() => setShowNav(!showNav)}
+                          role="button"
+                          onKeyDown={() => setShowNav(!showNav)}
+                          tabIndex={0}>Projects</Link></li>
+                <li><Link to='/contact'  onClick={() => setShowNav(!showNav)}
+                          role="button"
+                          onKeyDown={() => setShowNav(!showNav)}
+                          tabIndex={0}>Contact</Link></li>
             </ul>
         </NavMenuStyle>
     )
